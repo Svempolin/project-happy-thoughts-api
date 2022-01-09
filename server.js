d432acd465d5a9567b9415cfb1af524bffc8ae6b
Charlotte Nyman <85078175+Svempolin@users.noreply.github.com>
2022-01-09 22:32:31 +0100

    code the last bit of the project

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-import express from "express";
+import express, { response } from "express";
 import cors from "cors";
 import mongoose from "mongoose";
+import { restart } from "nodemon";
 
 const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/happyThoughts";
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
@@ -22,7 +23,7 @@ const ThoughtSchema = new mongoose.Schema({
   description: {
     type: String,
     minlength: 2,
-    maxlength: 15,
+    maxlength: 35,
     trim: true,
   },
   score: {
@@ -42,8 +43,21 @@ app.use(cors());
 app.use(express.json());
 
 // Start defining your routes here
-app.get("/", (req, res) => {
-  res.send("Hello world");
+app.get("/thought", async (req, res) => {
+  const {
+    sort,
+    page,
+    perPage,
+    sortNum = Number(sort),
+    pageNum = Number(page),
+    perPageNum = Number(perPage),
+  } = req.query;
+
+  const thought = await Thought.find({})
+    .sort({ createdAt: sortNum })
+    .skip((pageNum - 1) * perPageNum)
+    .limit(perPageNum);
+  res.status(200).json({ response: thought, success: true });
 });
 
 app.post("/thought", async (req, res) => {
@@ -59,14 +73,55 @@ app.post("/thought/:id/score", async (req, res) => {
   const { id } = req.params;
 
   try {
-    const updatedThought = await Thought.findByIdAndUpdate(id, {
-      $inc: {
-        score: 1,
+    const updatedThought = await Thought.findByIdAndUpdate(
+      id,
+      {
+        $inc: {
+          score: 1,
+        },
       },
-    });
+      {
+        new: true,
+      }
+    );
     res.status(200).json({ response: updatedThought, success: true });
   } catch (error) {
     res.status(400).json({ response: "could not update", success: false });
+  }
+});
+
+app.delete("/thought/:id", async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const deleteThought = await Thought.findOneAndDelete({ _id: id });
+    if (deleteThought) {
+      res.status(200).json({ response: deleteThought, success: true });
+    } else {
+      res.status(404).json({ response: "Member not found", success: false });
+    }
+  } catch (error) {
+    res.status(400).json({ response: error, success: false });
+  }
+});
+
+app.patch("/thought/:id", async (req, res) => {
+  const { id } = req.params;
+  const { name } = req.body;
+
+  try {
+    const updatedThought = await Thought.findOneAndUpdate(
+      { _id: id },
+      { name },
+      { new: true }
+    );
+    if (updatedThought) {
+      res.status(200).json({ response: updatedThought, success: true });
+    } else {
+      res.status(404).json({ response: "Member not found", success: false });
+    }
+  } catch (error) {
+    res.status(400).json({ response: error, success: false });
   }
 });
 
